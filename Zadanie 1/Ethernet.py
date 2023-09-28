551007c39070eb9c5b32f4e960e329edc2769d97
Lukáš Lovás <xlovasl@stuba.sk>
2023-09-28 12:47:26 +0200

    update

--- Zadanie 1/Ethernet.py
+++ Zadanie 1/Ethernet.py
@@ -2,6 +2,5 @@ from Output import Output
 
 
 class Ethernet(Output):
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, number, lenght, frametype, destination, source, data):
+        super().__init__(number, lenght, frametype, destination, source, data)
--- Zadanie 1/Zadanie 1.py
+++ Zadanie 1/Zadanie 1.py
@@ -1,6 +1,6 @@
-from binascii import hexlify
-
 from scapy.all import *
+
+from Ethernet import Ethernet
 
 
 def parse_pcap_file(pcap_path):
@@ -12,22 +12,27 @@ def parse_pcap_file(pcap_path):
     return frames
 
 
-def check_frametype(frame):  # TODO funkcia ktora rozhodne ci Ethertype alebo IEEE
+def diferentiate_frametype(frame, number, lenght):  # TODO funkcia ktora rozhodne ci Ethertype alebo IEEE
     # - spojit dva byte a porovnat ci je
     # vacsi alebo mensi ako 1500
-    frametype_bytes = frame[12:16]
-    print(frame)
-    print(frametype_bytes)
+    frametype_bytes = int(frame[24:26] + frame[26:28], 16)
+
+    if frametype_bytes >= 1536:
+        pass
 
 
-def check_hexcode(frame):
+def get_basic_params(frame):  # TODO dozistit ako zistim frame_medium_lenght
     number = frames.index(frame) + 1
-    frame = frame[6:]  # odstranenie preambuly
     frame = frame.replace(" ", "")
     lenght = len(frame)
-    frametype = check_frametype(frame)
+    destination = frame[0:13], 16
+    source = frame[13:25], 16
+    print(frame)
+    print(str(destination) + "\n" + str(source))
+    print(frame[0:13] + "\n" + frame[13:25])
+    diferentiate_frametype(frame, number, lenght)
 
 
 if __name__ == "__main__":
-    frames = parse_pcap_file("C:\eth-1.pcap")
-    check_hexcode(frames[0])
+    frames = parse_pcap_file("C:\\Users\Lukáš\Documents\Repozitare git\\3. Semester\PKS\Zadanie 1\\test_pcap_files\\vzorky_pcap_na_analyzu\eth-1.pcap")
+    get_basic_params(frames[0])
